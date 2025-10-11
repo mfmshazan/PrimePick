26d2caf39c08aadfe2bfbbfba340a50d1a6be663
mfmshazan <shazanmfm.23@uom.lk>
2025-10-11 22:59:45 +0530

    feat: add to-regex-range package with initial setup
    feat: add LICENSE and README for touch package
    
    feat: implement nodetouch CLI for touch package
    
    feat: add touch functionality in index.js for touch package
    
    chore: update package.json for touch package
    
    ci: add release workflow for undefsafe package
    
    chore: add .jscsrc and .jshintrc for undefsafe package
    
    ci: add .travis.yml for undefsafe package
    
    feat: add LICENSE and README for undefsafe package
    
    feat: implement undefsafe functionality in lib/undefsafe.js
    
    chore: update package.json for undefsafe package

---
+++ backend/config/db.js
@@ -0,0 +1,10 @@
+export const connectDB = async () => {
+    try{
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDB connect ${conn.connection.host}`);
+    }
+    catch(error){
+        console.log(`Error : ${error.message}`);
+        process.exit(1)
+    }
+}
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,14 @@
 import express from "express";
+import dotenv, { configDotenv } from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
+app.get("/products",(req,res) => {})
+
+console.log(process.env.MONGO_URI);
+
 app.listen(5000, () => {
     console.log(`server is running on https://localhost:5000`);
-})
+}) 
